ec8a40ce3a58e3f0c1ce5b194dbfe158d556111f
AT <ameropa.logistics@gmail.com>
2018-08-24 12:55:53 +0200

    new files for xls

--- app/models/fuel_expense.rb
+++ app/models/fuel_expense.rb
@@ -1,19 +1,24 @@
 class FuelExpense < ApplicationRecord
  require 'csv'
  belongs_to :truck, :optional => true
- validates_uniqueness_of :trsdate, scope: %i[product truck_id]
+ validates_uniqueness_of :trsdate, scope: %i[product truck_id trstime stationname]
 
  #CSV.read(file.path, :quote_char => "\´")
  def self.import(file)
     # a block that runs through a loop in our CSV data
-     CSV.foreach(file.path, headers: true, encoding: "ISO8859-1:utf-8", 
+     CSV.foreach(file.path, headers: true, encoding: "ISO-8859-1",  
                            :col_sep => ";", 
                            :quote_char => "\"", 
-                           :header_converters => lambda { |h| h.try(:downcase).try(:gsub,' ', '') }
+                           :header_converters => lambda { |h| h.try(:downcase).try(:gsub,' ', '').try(:gsub,'ï»¿', '') }
                 ) do |row| 
-        @my_truck = Truck.find_by NB_PLATE:row.to_a[6][1]
-        @my_row = row.to_a<<(["truck_id",@my_truck.id]) 
- 	 	FuelExpense.create! @my_row.to_h
+        @my_truck = Truck.find_by NB_PLATE:row.to_a[6][1].try(:gsub,' ', '')
+        if @my_truck != nil
+        	@my_row = row.to_a<<(["truck_id",@my_truck.id]) 
+ 	 		FuelExpense.create! @my_row.to_h
+ 	 	else
+ 	 		@my_row = row.to_a<<(["truck_id",17.to_i]) 
+ 	 		FuelExpense.create! @my_row.to_h
+ 	 	end	
      end
  end
  
