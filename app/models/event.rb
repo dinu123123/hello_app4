7bdd3fca03cd996e81ab436a781386b1760b8f88
AT <ameropa.logistics@gmail.com>
2018-08-25 13:49:38 +0200

    width

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,6 @@ class Event < ApplicationRecord
 belongs_to :driver, :optional => true
 belongs_to :truck, :optional => true
 belongs_to :client, :required => true
-validates_uniqueness_of :DRIVER_id, scope: %i[DATE]
+validates_uniqueness_of :DRIVER_id, scope: %i[DATE START_END]
 
 end
