fefe98d25585310ae3e136f334ce332a9571aec5
AT <ameropa.logistics@gmail.com>
2018-09-16 23:04:05 +0200

    fixed datetime in calculate individual

--- app/models/transaction_search.rb
+++ app/models/transaction_search.rb
@@ -375,8 +375,8 @@ else
             @germanyTollExpenses = DeToll.find_by_sql(['SELECT * FROM de_tolls where 
                 ((de_tolls.date > DATE(?) OR (de_tolls.date == DATE(?) AND TIME(de_tolls.time) >= TIME(?) )) 
                 AND ((de_tolls.date < DATE(?) OR (de_tolls.date == DATE(?) AND TIME(de_tolls.time)  <= TIME(?) )) 
-                ORDER BY de_tolls.date ASC, de_tolls.time ASC', @date_from, @date_from, @date_from,
-                @date_to, @date_to, @date_to ])
+                ORDER BY de_tolls.date ASC, de_tolls.time ASC', @date_from.to_date, @date_from.to_date, @date_from,
+                @date_to.to_date, @date_to.to_date, @date_to ])
             
             0.upto( @germanyTollExpenses.size-1) do |j|
                 @germanyTollExpenses[j].via =  @germanyTollExpenses[j].via[0,10]
