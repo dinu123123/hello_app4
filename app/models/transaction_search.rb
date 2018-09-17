6fc6ff8c6da722330310a6c8922da61683db65d6
AT <ameropa.logistics@gmail.com>
2018-09-17 08:53:00 +0200

    fixed datetime in calculate individual

--- app/models/transaction_search.rb
+++ app/models/transaction_search.rb
@@ -383,20 +383,22 @@ else
          if DeToll.all.size
 
 
- @germanyTollExpenses = DeToll.find_by_sql(['SELECT * FROM de_tolls where 
-                de_tolls.date > ?', to_date(@date_from) ])
+# @germanyTollExpenses = DeToll.find_by_sql(['SELECT * FROM de_tolls where 
+ #               de_tolls.date > ?) ', to_date(@date_from) ])
             
 
 
 
 
-sd
-#            @germanyTollExpenses = DeToll.find_by_sql(['SELECT * FROM de_tolls where 
-#                ((de_tolls.date > ?) OR (de_tolls.date == ? AND de_tolls.time >= ? )) 
-#                AND ((de_tolls.date < ?) OR (de_tolls.date == ? AND de_tolls.time  <= ? )) 
-#                ORDER BY de_tolls.date ASC, de_tolls.time ASC', to_date(@date_from), to_date(@date_from), to_time(@date_from),
-#                to_date(@date_to), to_date(@date_to), to_time(@date_to) ])
+
+
+           @germanyTollExpenses = DeToll.find_by_sql(['SELECT * FROM de_tolls where 
+                ( de_tolls.date > ?  OR (de_tolls.date == ? AND de_tolls.time >= ? )) 
+                AND ( de_tolls.date < ? OR (de_tolls.date == ? AND de_tolls.time  <= ? )) 
+                ORDER BY de_tolls.date ASC, de_tolls.time ASC', to_date(@date_from), to_date(@date_from), to_time(@date_from),
+                to_date(@date_to), to_date(@date_to), to_time(@date_to) ])
             
+          
           
 
 
