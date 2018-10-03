8298058cd2ba6477fae98534bed96510fc807ed3
AT <ameropa.logistics@gmail.com>
2018-10-03 21:10:50 +0200

    fixed help

--- app/models/driver_expense.rb
+++ app/models/driver_expense.rb
@@ -1,5 +1,5 @@
 class DriverExpense < ApplicationRecord
   belongs_to :driver, :optional => true
   belongs_to :truck, :optional => true
-  validates_uniqueness_of :DRIVER_id, scope: %i[DATE AMOUNT]
+  validates_uniqueness_of :DRIVER_id, scope: %i[DATE AMOUNT DESCRIPTION]
 end
