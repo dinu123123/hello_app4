c3400fe8ffb85360cae026f2e78d25f31fababfb
AT <ameropa.logistics@gmail.com>
2018-10-07 00:12:23 +0200

    fixed events selection index display

--- app/controllers/driver_expenses_controller.rb
+++ app/controllers/driver_expenses_controller.rb
@@ -27,7 +27,7 @@ end
                    driver_expenses."DATE" BETWEEN ? AND ? ORDER BY driver_expenses."DATE" DESC, driver_expenses."DRIVER_id" ASC ', 
                    event_to_date(@search.date_from), event_to_date(@search.date_to)])
     else
-         @driver_expenses = DriverExpense.find_by_sql(['SELECT * FROM driver_expenses where driver_expenses.truck_id = ? AND
+         @driver_expenses = DriverExpense.find_by_sql(['SELECT * FROM driver_expenses where driver_expenses."DRIVER_id" = ? AND
                    driver_expenses."DATE" BETWEEN ? AND ? ORDER BY driver_expenses."DATE" DESC, driver_expenses."DRIVER_id" ASC ', 
                   @search.driver_id, event_to_date(@search.date_from), event_to_date(@search.date_to)])
     end
