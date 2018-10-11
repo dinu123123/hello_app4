5ec09276fcc34feba7467d0c3012004172a53495
AT <ameropa.logistics@gmail.com>
2018-10-11 22:34:05 +0200

    fixed display select for trips

--- app/models/invoiced_trip.rb
+++ app/models/invoiced_trip.rb
@@ -4,6 +4,6 @@ belongs_to :truck, :optional => true
 belongs_to :client, :required => true
 belongs_to :invoice, :required => true
 
-validates_uniqueness_of :client_id, scope: %i[StartDate EndDate DRIVER_id]
+#validates_uniqueness_of :client_id, scope: %i[StartDate EndDate DRIVER_id]
 validates_uniqueness_of :client_id, scope: %i[StartDate EndDate truck_id]
 end
