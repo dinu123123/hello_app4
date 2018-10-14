986295510c0c072ee713cfd784cba396a8f80cf0
AT <ameropa.logistics@gmail.com>
2018-10-14 23:32:40 +0200

    fixed display select for trips

--- app/models/periodic_transaction_search.rb
+++ app/models/periodic_transaction_search.rb
@@ -7,7 +7,7 @@ class PeriodicTransactionSearch
     params ||= {}
     @date_from = parsed_date(params[:date_from], 30.days.ago.to_date.to_s)
 
-    @date_to = parsed_date(params[:date_to],  Date.today.to_s)
+    @date_to = parsed_date(params[:date_to],  (Date.today+1.month).to_s)
     
 
     @time = parsed_time_interval(params[:interval], 1)
