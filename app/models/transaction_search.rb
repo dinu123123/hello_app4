bf9765a64e5b9d3389e9b47e31c37ba3e19f8c75
AT <ameropa.logistics@gmail.com>
2018-10-14 22:38:00 +0200

    fixed display select for trips

--- app/models/transaction_search.rb
+++ app/models/transaction_search.rb
@@ -5,7 +5,7 @@ class TransactionSearch
     params ||= {}
     @date_from = parsed_date(params[:date_from],30.days.ago.to_datetime.strftime('%Y-%m-%dT%H:%M') )
 
-    @date_to = parsed_date(params[:date_to],  DateTime.now.strftime('%Y-%m-%dT%H:%M') )
+    @date_to = parsed_date(params[:date_to],  (DateTime.now+1.day).strftime('%Y-%m-%dT%H:%M') )
     @driver_id = parsed_driver_id(params[:driver_id], 1)
     @truck_id = parsed_truck_id(params[:truck_id], 1)
     @client_id = parsed_client_id(params[:client_id], 1)
