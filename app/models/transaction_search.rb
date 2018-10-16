0ad0572d1907084ab1714232025b7682338474ab
AT <ameropa.logistics@gmail.com>
2018-10-16 23:39:54 +0200

    fixed display select for trips

--- app/models/transaction_search.rb
+++ app/models/transaction_search.rb
@@ -58,46 +58,46 @@ if @client_id > 0
 
       if @driver_id > 0 and @truck_id == 0
                     @invoiced_trips = InvoicedTrip.find_by_sql(['SELECT * FROM invoiced_trips where invoiced_trips.client_id = ? and invoiced_trips."DRIVER_id" = ? 
-                      and invoiced_trips.date BETWEEN ? AND ? ORDER BY invoiced_trips.invoice_id DESC', @client_id,
+                      and invoiced_trips.date BETWEEN ? AND ? ORDER BY invoiced_trips.invoice_id DESC, invoiced_trips.client_id ASC, invoiced_trips."DRIVER_id', @client_id,
                       @driver_id, to_datetime(@date_from), to_datetime(@date_to)])
                      
 
             elsif @truck_id > 0 && @driver_id == 0
                      @invoiced_trips = InvoicedTrip.find_by_sql(['SELECT * FROM invoiced_trips where invoiced_trips.client_id = ? and invoiced_trips.truck_id = ? 
-                          and invoiced_trips.date BETWEEN ? AND ? ORDER BY invoiced_trips.invoice_id DESC', @client_id,
+                          and invoiced_trips.date BETWEEN ? AND ? ORDER BY invoiced_trips.invoice_id DESC, invoiced_trips.client_id ASC, invoiced_trips."DRIVER_id', @client_id,
                           @truck_id, to_datetime(@date_from), to_datetime(@date_to)])
                      
             elsif @truck_id > 0 && @driver_id > 0
                       @invoiced_trips = InvoicedTrip.find_by_sql(['SELECT * FROM invoiced_trips where invoiced_trips.client_id = ? and invoiced_trips."DRIVER_id" = ? 
-                        and invoiced_trips.truck_id = ? and invoiced_trips.date BETWEEN ? AND ? ORDER BY invoiced_trips.invoice_id DESC', 
+                        and invoiced_trips.truck_id = ? and invoiced_trips.date BETWEEN ? AND ? ORDER BY invoiced_trips.invoice_id DESC, invoiced_trips.client_id ASC, invoiced_trips."DRIVER_id', 
                         @client_id, @driver_id, @truck_id, to_datetime(@date_from), to_datetime(@date_to)])
                      
             else
                         @invoiced_trips = InvoicedTrip.find_by_sql(['SELECT * FROM invoiced_trips where invoiced_trips.client_id = ? and invoiced_trips.date BETWEEN ? 
-                          AND ? ORDER BY invoiced_trips.invoice_id DESC', @client_id, to_datetime(@date_from), to_datetime(@date_to)])
+                          AND ? ORDER BY invoiced_trips.invoice_id DESC, invoiced_trips.client_id ASC, invoiced_trips."DRIVER_id" ASC', @client_id, to_datetime(@date_from), to_datetime(@date_to)])
                       
             end
 
 else 
       if @driver_id > 0 and @truck_id == 0
               @invoiced_trips = InvoicedTrip.find_by_sql(['SELECT * FROM invoiced_trips where invoiced_trips."DRIVER_id" = ? 
-                and invoiced_trips.date BETWEEN ? AND ? ORDER BY invoiced_trips.invoice_id DESC', 
+                and invoiced_trips.date BETWEEN ? AND ? ORDER BY invoiced_trips.invoice_id DESC, invoiced_trips.client_id ASC, invoiced_trips."DRIVER_id" ASC', 
                 @driver_id, to_datetime(@date_from), to_datetime(@date_to)])
                
       elsif @truck_id > 0 && @driver_id == 0
                @invoiced_trips = InvoicedTrip.find_by_sql(['SELECT * FROM invoiced_trips where invoiced_trips.truck_id = ? 
-                    and invoiced_trips.date BETWEEN ? AND ? ORDER BY invoiced_trips.invoice_id DESC', 
+                    and invoiced_trips.date BETWEEN ? AND ? ORDER BY invoiced_trips.invoice_id DESC, invoiced_trips.client_id ASC, invoiced_trips."DRIVER_id" ASC', 
                     @truck_id, to_datetime(@date_from), to_datetime(@date_to)])
                
       elsif @truck_id > 0 && @driver_id > 0
                 @invoiced_trips = InvoicedTrip.find_by_sql(['SELECT * FROM invoiced_trips where invoiced_trips."DRIVER_id" = ? and invoiced_trips.truck_id = ? 
-                    and invoiced_trips.date BETWEEN ? AND ? ORDER BY invoiced_trips.invoice_id DESC', 
+                    and invoiced_trips.date BETWEEN ? AND ? ORDER BY invoiced_trips.invoice_id DESC, invoiced_trips.client_id ASC, invoiced_trips."DRIVER_id" ASC', 
                     @driver_id, @truck_id, to_datetime(@date_from), to_datetime(@date_to)])
                
       else
         
                   @invoiced_trips = InvoicedTrip.find_by_sql(['SELECT * FROM invoiced_trips where invoiced_trips.date BETWEEN ? 
-                    AND ? ORDER BY invoiced_trips.invoice_id DESC', to_datetime(@date_from), to_datetime(@date_to)])
+                    AND ? ORDER BY invoiced_trips.invoice_id DESC, invoiced_trips.client_id ASC, invoiced_trips."DRIVER_id" ASC', to_datetime(@date_from), to_datetime(@date_to)])
                 
       end
 end
