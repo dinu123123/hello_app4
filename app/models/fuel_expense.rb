9b6acac6cb35ce092ea62f1388999508ed76b9d6
AT <ameropa.logistics@gmail.com>
2018-10-26 23:33:55 +0200

    fixed display select for trips

--- app/models/fuel_expense.rb
+++ app/models/fuel_expense.rb
@@ -1,9 +1,7 @@
 class FuelExpense < ApplicationRecord
  require 'csv'
  belongs_to :truck, :optional => true
-
-
-#validates :trstime, uniqueness: {scope: [:product, :truck_id, :trsdate, :stationname]}
+ #validates_uniqueness_of :trstime, scope: %i[product truck_id trsdate stationname]
 
  #CSV.read(file.path, :quote_char => "\´")
  def self.import(file)
