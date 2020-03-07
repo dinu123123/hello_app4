67cdcd5576ea13451a55258f25f3d3039196a4a5
AT <ameropa.logistics@gmail.com>
2020-03-08 00:21:42 +0100

    added ordered invoices

--- app/models/transaction_search.rb
+++ app/models/transaction_search.rb
@@ -6,7 +6,7 @@ class TransactionSearch
   def initialize(params, large =false)
 
     params ||= {}
-    @date_from = parsed_date(params[:date_from],(DateTime.now - 1.month).strftime('%Y-%m-%dT%H:%M') )
+    @date_from = parsed_date(params[:date_from],(DateTime.now - 10.days).strftime('%Y-%m-%dT%H:%M') )
 
     @date_to = parsed_date(params[:date_to],  (DateTime.now+1.day).strftime('%Y-%m-%dT%H:%M') )
     @driver_id = parsed_driver_id(params[:driver_id], 1)
