2b628016144cd34b4525772632fc905e2285085e
AT <ameropa.logistics@gmail.com>
2020-09-30 22:51:57 +0200

    added ordered invoices

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -499,7 +499,7 @@ def weekly
                     
                         
                            @localEvent.each_with_index do |event,i|
-                             if(event.expected_date.strftime("%U").to_i >= period_start and event.expected_date.strftime("%U").to_i <= period_end )
+                             if(event.expected_date and event.expected_date.strftime("%U").to_i >= period_start and event.expected_date.strftime("%U").to_i <= period_end )
                                @tmpEvent = Struct.new(:date, :type)
                                @tmp = @tmpEvent.new(event.DATE, event.START_END)
                               
