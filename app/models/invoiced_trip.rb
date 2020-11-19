6c6b309f17e6008d22f632a3c41c613f8cf6d1e2
AT <ameropa.logistics@gmail.com>
2020-11-19 17:45:32 +0100

    added ordered invoices

--- app/models/invoiced_trip.rb
+++ app/models/invoiced_trip.rb
@@ -4,8 +4,9 @@ belongs_to :truck, :optional => true
 belongs_to :client, :required => true
 belongs_to :invoice, :required => true
 
-#validates_uniqueness_of :client_id, scope: %i[StartDate EndDate DRIVER_id]	
-validates_uniqueness_of :invoice_id
+validates_uniqueness_of :client_id, scope: %i[invoice_id StartDate EndDate DRIVER_id]	, if: -> { typeT == nil or typeT == 0 } 
+
+validates_uniqueness_of :invoice_id , if: -> { name.present? } , if: -> { not( typeT == nil or typeT == 0) } 
 
 
 
