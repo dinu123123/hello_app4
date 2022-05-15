e07cb5731da57a7006134233ab2f7c0f20436fce
aalex <aturjan@yahoo.com>
2022-05-15 11:20:58 +0200

    added ordered invoices

--- db/migrate/20220427132214_add_ddate_to_invoices.rb
+++ db/migrate/20220427132214_add_ddate_to_invoices.rb
@@ -1,5 +1,5 @@
 class AddDdateToInvoices < ActiveRecord::Migration[5.2]
   def change
-    add_column :invoices, :ddate, :date
+    add_column :invoices, :ddate, :date, :default => '01/01/2000'
   end
 end
