36bf723a557656ffb784bc1f5225c214a3cf1b37
aalex <aturjan@yahoo.com>
2022-08-22 18:53:42 +0200

    added ordered invoices

--- app/models/generic_toll.rb
+++ app/models/generic_toll.rb
@@ -1,4 +1,4 @@
 class GenericToll < ApplicationRecord
   belongs_to :truck, :optional => true
-  validates_uniqueness_of :StartDate, scope: %i[Km truck_id EUR]
+  #validates_uniqueness_of :StartDate, scope: %i[Km truck_id EUR]
 end
